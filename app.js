ec059422dbbf6a16aa50e9c6769c2d796250cdb2
Jeffery Cosby <jcoz1968@hotmail.com>
2019-01-26 16:08:50 -0600

    return books

--- app.js
+++ app.js
@@ -10,14 +10,19 @@ const Book = require('./models/bookModel');
 bookRouter.route('/books')
   .get((req, res) => {
     const response = { hello: 'This is my API' };
-    res.json(response);
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
 app.use('/api', bookRouter);
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Nodemon API!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my Nodemon API!');
+// });
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
